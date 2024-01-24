f61ddbdeff494dc44d9e4b4ffa26e2dccdbe71d5
RM349626@fiap.com.br <RM349626@fiap.com.br>
2024-01-23 21:09:50 -0300

    integration tests movie controller

--- src/test/java/com/fiap/alegorflix/movie/controller/MovieControllerIT.java
+++ src/test/java/com/fiap/alegorflix/movie/controller/MovieControllerIT.java
@@ -1,11 +1,10 @@
 package com.fiap.alegorflix.movie.controller;
 
+import com.fiap.alegorflix.movie.entity.Movie;
 import com.fiap.alegorflix.utils.MovieHelper;
 import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.RestAssured;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,12 +13,15 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
+import java.util.List;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasKey;
 
 @AutoConfigureDataMongo
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestClassOrder(ClassOrderer.OrderAnnotation.class)
 public class MovieControllerIT {
     @LocalServerPort
     private int port;
@@ -34,6 +36,7 @@ public class MovieControllerIT {
     }
 
     @Nested
+    @Order(1)
     class CreateMovie {
 
         @Test
@@ -55,5 +58,67 @@ public class MovieControllerIT {
                 .body("description", equalTo(movieRequest.description()));
 
         }
+
+        @Test
+        void shouldNotCreateAMovieWhenMovieAlreadyExist() {
+            var movieRequest = MovieHelper.createMovieDTO();
+
+            given()
+                .filter(new AllureRestAssured())
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(movieRequest)
+                .when()
+                .post("/movies")
+                .then()
+                .statusCode(HttpStatus.CONFLICT.value())
+                .body("$", hasKey("message"))
+                .body("message", equalTo("Movie with title 'Scream' already exists"));
+        }
     }
+
+    @Nested
+    @Order(2)
+    class FindMovie {
+
+        @Test
+        void shouldFindAllMovies() {
+
+            given()
+                .filter(new AllureRestAssured())
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .queryParam("page", 0, "size", 10, "sortBy", "publishedDate")
+                .when()
+                .get("/movies")
+                .then()
+                .statusCode(HttpStatus.OK.value());
+        }
+
+        @Test
+        void shouldFindAMovieById() {
+            List<Movie> movieList = mongoTemplate.findAll(Movie.class);
+
+            given()
+                .filter(new AllureRestAssured())
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .get("/movies/{id}", movieList.get(0).getId())
+                .then()
+                .statusCode(HttpStatus.OK.value());
+
+        }
+
+        @Test
+        void shouldNotFindAMovieIdNotFound() {
+
+            given()
+                .filter(new AllureRestAssured())
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .get("/movies/{id}","2")
+                .then()
+                .statusCode(HttpStatus.NOT_FOUND.value());
+
+        }
+    }
+
 }
